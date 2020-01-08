4fc7963f64d3fe7c650efc3eb4b69ff77feb3b9b
Surajit Basak <surajitbasak109@gmail.com>
2020-01-08 23:46:36 +0530

    Looping through lists

---
+++ Chapters/C12 - Looping through lists/app.js
@@ -0,0 +1,14 @@
+new Vue({
+  el: "#app",
+  data: {
+    name: "Suman",
+    characters: ["Avijit", "Moloy", "Bapi", "Rajesh"],
+    ninjas: [
+      { name: "Hitesh", age: 25 },
+      { name: "Manju", age: 18 },
+      { name: "Paltu", age: 48 }
+    ]
+  },
+  methods: {},
+  computed: {}
+});

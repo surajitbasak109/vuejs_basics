4b0d568c8e6c1e4c035a007e9d5def2ed3fb7d27
Surajit Basak <surajitbasak109@gmail.com>
2020-01-11 22:38:31 +0530

    Form input binding - Creating a Blog - 1

---
+++ cli/vuejs/src/components/addBlog.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="add-blog">
+    <h2>Add a new blog post</h2>
+    <form>
+      <label>Blog Title: </label>
+      <input type="text" v-model.lazy="blog.title" required />
+      <label>Blog Content:</label>
+      <textarea v-model.lazy="blog.content"></textarea>
+    </form>
+    <div class="preview">
+      <h3>Preview Blog</h3>
+      <p>Blog Title: {{ blog.title }}</p>
+      <p>Blog Content:</p>
+      <p>{{ blog.content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    data() {
+      return {
+        blog: {
+          title: "",
+          content: ""
+        }
+      }
+    },
+    methods: {}
+  };
+</script>
+
+<style>
+  .add-blog * {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+  .add-blog {
+    margin: 20px auto;
+    max-width: 500px;
+  }
+  label {
+    display: block;
+    margin: 20px 0 10px;
+  }
+  input[type="text"], textarea {
+    display: block;
+    width: 100%;
+    padding: 8px;
+  }
+  .preview {
+    padding: 10px 20px;
+    border: 1px dotted #cccccc;
+    margin: 30px 0;
+  }
+  h3 {
+    margin-top: 10px;
+  }
+</style>

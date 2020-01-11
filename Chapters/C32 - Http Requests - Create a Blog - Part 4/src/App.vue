99f5e4506fdedd042920f427c9b30bfdc17dde13
Surajit Basak <surajitbasak109@gmail.com>
2020-01-12 00:31:22 +0530

    Http Requests - Create a Blog - 4

---
+++ Chapters/C32 - Http Requests - Create a Blog - Part 4/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container mt-3">
+    <add-blog></add-blog>
+  </div>
+</template>
+
+<script>
+  import addBlog from './components/addBlog';
+  export default {
+    components: {
+      'add-blog': addBlog
+    },
+    data() {
+      return {}
+    },
+    methods: {}
+  };
+</script>
+
+<style>
+  body {
+    margin: 0;
+  }
+</style>

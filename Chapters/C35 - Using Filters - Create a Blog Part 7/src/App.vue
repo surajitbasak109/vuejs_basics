24eb8f444358e6c9bf514424c1078cbf02bb4c95
Surajit Basak <surajitbasak109@gmail.com>
2020-01-12 15:50:18 +0530

    Using Filters - Create a Blog - 7

---
+++ Chapters/C35 - Using Filters - Create a Blog Part 7/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container mt-3">
+    <show-blogs></show-blogs>
+  </div>
+</template>
+
+<script>
+  import addBlog from './components/addBlog';
+  import showBlogs from "./components/showBlogs";
+
+  export default {
+    components: {
+      'show-blogs': showBlogs
+    },
+    data() {
+      return {}
+    },
+    methods: {}
+  };
+</script>
+
+<style>
+  body {
+    margin: 0;
+  }
+</style>

cdea29466d36b05b64bf4bca15f4f2b02d93384d
Surajit Basak <surajitbasak109@gmail.com>
2020-01-12 17:00:25 +0530

    Enable History Mode - Create a Blog - 12

---
+++ Chapters/C40 - Enable History Mode - Create a Blog - Part 12/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container mt-3">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import addBlog from './components/addBlog';
+  import showBlogs from "./components/showBlogs";
+  import listBlogs from "./components/listBlogs";
+
+  export default {
+    components: {
+      'show-blogs': showBlogs,
+      'list-blogs': listBlogs
+    },
+    data() {
+      return {}
+    },
+    methods: {}
+  };
+</script>
+
+<style>
+  body {
+    margin: 0;
+  }
+</style>
